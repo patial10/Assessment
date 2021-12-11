9987f5ad16c453ae6474a8f788dc0478712ef9e3
patial10 <75527076+patial10@users.noreply.github.com>
2021-12-11 13:13:48 +0530

    Add files via upload

---
+++ db1.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2021 at 08:42 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb1`
+--
+
+CREATE TABLE `tb1` (
+  `text1` text NOT NULL,
+  `status` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb1`
+--
+
+INSERT INTO `tb1` (`text1`, `status`) VALUES
+('0.txt', 1),
+('1.txt', 1),
+('2.txt', 1),
+('3.txt', 1),
+('4.txt', 1),
+('5.txt', 1),
+('6.txt', 1),
+('7.txt', 1),
+('8.txt', 1),
+('9.txt', 1),
+('10.txt', 1),
+('11.txt', 1),
+('12.txt', 1),
+('13.txt', 1),
+('14.txt', 1),
+('15.txt', 1),
+('16.txt', 1),
+('17.txt', 1),
+('18.txt', 1),
+('19.txt', 1),
+('20.txt', 1),
+('21.txt', 1),
+('22.txt', 1),
+('23.txt', 1),
+('24.txt', 1),
+('25.txt', 1),
+('26.txt', 1),
+('27.txt', 1),
+('28.txt', 1),
+('29.txt', 1),
+('30.txt', 1),
+('31.txt', 1),
+('32.txt', 1),
+('33.txt', 1),
+('34.txt', 1),
+('35.txt', 1),
+('36.txt', 1),
+('37.txt', 1),
+('38.txt', 1),
+('39.txt', 1),
+('40.txt', 1),
+('41.txt', 1),
+('42.txt', 1),
+('43.txt', 1),
+('44.txt', 1),
+('45.txt', 1),
+('46.txt', 1),
+('47.txt', 1),
+('48.txt', 1),
+('49.txt', 1),
+('50.txt', 0),
+('51.txt', 0),
+('52.txt', 0),
+('53.txt', 0),
+('54.txt', 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
